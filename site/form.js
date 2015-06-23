8d0a2e5a3e21f62486625b461a7b6c38f45adaeb
Philip A. Hirt <phirt13@gmail.com>
2015-06-23 15:04:21 -0700

    converted native to jQuery

--- site/form.js
+++ site/form.js
@@ -1,15 +1,37 @@
-var Product = document.getElementById('product');
-var ProductMessage = document.getElementById('productMessage');
+// Vanilla Js========================
 
-function productMessage() {
-  var product = this.options[this.selectedIndex].value;
-  if (product !== '') {
-    ProductMessage.innerHTML = 'You have selected: ' + product;
-  }
-  else {
-    ProductMessage.innerHTML = 'Please choose a product!';
-  }
-}
+// var Product = document.getElementById('product');
+// var ProductMessage = document.getElementById('productMessage');
 
-Product.addEventListener('change', productMessage, false);
+// function productMessage() {
+//   var product = this.options[this.selectedIndex].value;
+//   if (product !== '') {
+//     ProductMessage.innerHTML = 'You have selected: ' + product;
+//   }
+//   else {
+//     ProductMessage.innerHTML = 'Please choose a product!';
+//   }
+// }
 
+// Product.addEventListener('change', productMessage, false);
+
+// jQuery========================
+
+$(function() {
+
+  var $product = $('#product');
+  var $productMessage = $('#productMessage');
+
+  $product.change(function() {
+
+    var $option = $("option:selected").val()
+
+    if($option !== '') {
+      $productMessage.text('You have selected: ' + $option);
+    } else {
+      $productMessage.text('Please choose a product')
+    }
+
+  });
+
+});
