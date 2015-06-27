f99a4d8117cb9f8b259e2936decd5b701f1934e3
Luke Gabrieli <usetheforce10@gmail.com>
2015-06-27 10:36:11 -0700

    added final api js files

--- site/js/ajax.js
+++ site/js/ajax.js
@@ -18,22 +18,3 @@ $('form').submit(function (evt) {
   $.getJSON(url, flickrPhotos, showPhotos);
 });
 
-// $('.btn').click(function() {
-//   var flickrAPI = "https://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-//   var options = {
-//     tags: "kitten",
-//     format: "json"
-//   };
-//   function showPhotos(data) {
-//     var photoList = '<ul>';
-//     $.each(data.items, function(i, photo) {
-//       photoList += '<li>';
-//       photoList += '<a href="' + photo.link + '">';
-//       photoList += '<img src="' + photo.media.m + '"></a></li>';
-//     });
-//     photoList += '</ul>';
-//     $('.container').html(photoList);
-//   }
-//   $.getJSON(flickrAPI, options, showPhotos);
-// });
-
